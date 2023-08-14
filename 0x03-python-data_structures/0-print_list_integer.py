eae81c4153ef4446e532742bae73ac109dbc6e96
Ndarugaa <ndaruga.francis@gmail.com>
2023-08-15 00:26:54 +0300

    check if list is empty

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print(str.format("{}", i))
+    if my_list:
+        for i in my_list:
+            print(str.format("{:d}", i))
+    else:
+        return None
     return None
