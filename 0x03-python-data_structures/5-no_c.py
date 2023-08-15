b973d792234d633bcd0f43856bc3128951714406
Ndarugaa <ndaruga.francis@gmail.com>
2023-08-15 03:54:04 +0300

    check if its a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = ""
-    for i in my_string:
-        if i == "c" or i == "C":
-            pass
-        else:
-            result+=i
-    return result
+    if my_string:
+        result = ""
+        for i in my_string:
+            if i == "c" or i == "C":
+                pass
+            else:
+                result+=i
+        return result
+    else:
+        return None
